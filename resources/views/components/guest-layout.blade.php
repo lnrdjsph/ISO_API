2b18c07b9bd9aaa967ed4bf4229021c1abdb83a3
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2025-08-18 17:04:09 +0800

    Fixed import mapping 08-18-2025

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }} - Login</title>
-    @vite('resources/css/app.css')
-</head>
-<body class="antialiased">
-    {{ $slot }}
-</body>
+
+		<head>
+				<meta charset="UTF-8">
+				<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+				>
+				<title>{{ config('app.name') }} - Login</title>
+				<link
+						rel="icon"
+						type="image/png"
+						href="{{ asset('images/MarengEms_Logo.png') }}"
+				>
+				@vite('resources/css/app.css')
+		</head>
+
+		<body class="antialiased">
+				{{ $slot }}
+		</body>
+
 </html>
