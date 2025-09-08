aa903e95706c580500be88f3fa7a8dd77ee7726b
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2025-09-08 14:21:13 +0800

    fix login 6

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -7,19 +7,9 @@ class LoginResponse implements LoginResponseContract
 {
     public function toResponse($request)
     {
-        // Get full APP_URL (with /iso-api)
-        $baseUrl = rtrim(config('app.url'), '/');
+        // Always rely on Fortify home
+        $defaultRedirect = config('fortify.home', '/');
 
-        // If there is an intended URL, prepend APP_URL
-        $intended = $request->session()->pull('url.intended');
-
-        if ($intended) {
-            // Prevent double slashes
-            $intended = ltrim($intended, '/');
-            return redirect()->to("{$baseUrl}/{$intended}");
-        }
-
-        // Default fallback
-        return redirect()->to($baseUrl);
+        return redirect()->intended($defaultRedirect);
     }
 }
