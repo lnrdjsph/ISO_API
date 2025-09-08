ffc05cb3975f005e2c80801db74d1245299cb273
lnrdjsph <leonardjoseph124@gmail.com>
2025-09-08 14:07:07 +0800

    fix login 2

---
+++ app/Http/Responses/UnauthenticatedResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\Request;
+
+class UnauthenticatedResponse
+{
+    public function toResponse(Request $request)
+    {
+        // Always send to iso-api login if session expired
+        return redirect('/iso-api/login');
+    }
+}
