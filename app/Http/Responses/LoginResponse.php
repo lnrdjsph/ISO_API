8c898ae5400ecb98d7bbaa1774f6505c940dac9a
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2025-09-08 13:58:21 +0800

    fix login

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -2,15 +2,14 @@
 namespace App\Http\Responses;
 
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
-use Illuminate\Http\Request;
 
 class LoginResponse implements LoginResponseContract
 {
     public function toResponse($request)
     {
-        // Use intended URL or fallback to FORTIFY_REDIRECT
-        $redirectTo = $request->session()->pull('url.intended', env('FORTIFY_REDIRECT', '/'));
-
-        return redirect()->to($redirectTo);
+        // Always respect intended URL first
+        return redirect()->intended(
+            config('fortify.home', '/iso-api')
+        );
     }
 }
