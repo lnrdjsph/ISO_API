805e0fad76f854aa27cb527b65672a02bf4339fb
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2025-12-19 14:44:44 +0800

     2025-12-19, QA testing 1

--- app/Http/Controllers/OracleTransferController.php
+++ app/Http/Controllers/OracleTransferController.php
@@ -386,7 +386,7 @@ public function getItemStatus($storeOrderNo)
                 ->where('distro_no', $storeOrderNo)
                 ->where('qty_received', '>', 0)
                 ->first();
-
+            Log::info('Items:', [$shipSku]);
             // If qty_received > 0, status becomes Received
             if ($shipSku) {
                 $status = 'Received';
