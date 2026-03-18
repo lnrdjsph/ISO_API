466398ea28fb834f81211dfd8357d51d1b2ef77d
lnrdjsph <leonardjoseph124@gmail.com>
2026-03-18 09:58:05 +0800

    Updated 03-18-2026 - Security Fixes 6

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,15 +3,27 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Support\Facades\Cookie;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array<int, string>
-     */
-    protected $except = [
-        //
-    ];
+    protected $except = [];
+
+    protected function newCookie($request, $config)
+    {
+        return Cookie::make(
+            'XSRF-TOKEN',
+            $this->encrypter->encrypt(
+                $request->session()->token(),
+                static::serialized()
+            ),
+            $config['lifetime'],
+            $config['path'],
+            $config['domain'],
+            $config['secure'],
+            true,  // ✅ HttpOnly = true
+            false,
+            $config['same_site'] ?? null
+        );
+    }
 }
